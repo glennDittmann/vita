336e8b6e17f891ddf188a69991174bf9a015aaea
glennDittmann <glenn.dittmann@posteo.de>
2025-05-17 15:05:27 +0200

    reduce boilerplate via combineReducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,13 +1,12 @@
+import { combineReducers } from "redux";
 import experienceReducer from "./features/experienceSettings/experienceSettingsSlice";
 import vertexSettingsReducer from "./features/vertexSettings/vertexSettingsSlice";
 
 
-const reducer = (state: any = {}, action: any) => {
-  return {
-    experienceSettings: experienceReducer(state.experienceSettings, action),
-    vertexSettings: vertexSettingsReducer(state.vertexSettings, action),
-  }
+const reducer = combineReducers({
+  experienceSettings: experienceReducer,
+  vertexSettings: vertexSettingsReducer,
 
-};
+});
 
 export default reducer;
